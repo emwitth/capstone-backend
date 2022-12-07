b2ca25e469836896a1809021fc4f28bdc4c33f27
Evan Witthun <emwitthun@gmail.com>
2022-12-07 06:05:42 -0600

    added ARP packet handle
    Signed-off-by: Evan Witthun <emwitthun@gmail.com>

--- constants.py
+++ constants.py
@@ -4,7 +4,8 @@
 
 # controls whether packet info is printed while it is sniffed
 # used for debugging
-PRINT_PACKET_INFO = False
+PRINT_PACKET_INFO = True
+PRINT_PACKET_HEX = False
 
 # for storing which role the packet is
 # also used for easy printing, that is why not bool
@@ -17,6 +18,9 @@ NO_PORT = "no port"
 NO_ROLE = "no role"
 NO_IP = "no ip"
 NO_HOSTNAME = "no hostname"
+
+# for names of non-process/ip nodes
+ARP_NODE_NAME = "ARP PACKETS"
 
 # for console printing
 LINE = "--------------------------------"
