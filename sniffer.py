63272d1b35a2a331b2d0030a64bd9333a0d283bb
Evan Witthun <emwitthun@gmail.com>
2023-02-10 21:20:45 -0600

    small fix to logic
    Signed-off-by: Evan Witthun <emwitthun@gmail.com>

--- sniffer.py
+++ sniffer.py
@@ -107,13 +107,15 @@ class PacketSniffer:
         # find process by id
         for proc in process_iter():
             if(proc.pid == id):
-                self.icmp_procs[id] = ProgInfo(proc.name(), NO_PORT, proc.pid)
+                if id in self.icmp_procs:
+                    self.icmp_procs[id].update_timestamp()
+                else:
+                    self.icmp_procs[id] = ProgInfo(proc.name(), NO_PORT, proc.pid)
             print(proc.pid)
         # deal with if not found process
         toReturn = ProgInfo(NO_PROC, NO_PORT, NO_PROC)
         if id in self.icmp_procs:
             toReturn = self.icmp_procs[id]
-            self.icmp_procs[id].update_timestamp()
         return toReturn
 
 
